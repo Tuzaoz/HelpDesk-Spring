d94562b7e5479133d61b9af90a0aa4ce62b09643
Arthur Dantas <arthurdantaspro@gmail.com>
2023-04-18 15:24:09 -0300

    Projeto Iniciado

---
+++ src/main/java/com/arthur/helpdeskspringangular/HelpdeskSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.helpdeskspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpdeskSpringAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arthur/helpdeskspringangular/HelpdeskSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.helpdeskspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskSpringAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
