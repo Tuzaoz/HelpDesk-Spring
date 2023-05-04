8ce8be300eb45f76d8b3a47dfe5fc987b4dfb173
Arthur Dantas <arthurdantaspro@gmail.com>
2023-05-04 19:47:49 -0300

    Criado Controller de tecnico e metodo findbyid

---
+++ src/main/java/com/arthur/helpdeskspringangular/services/TecnicoService.java
@@ -0,0 +1,19 @@
+package com.arthur.helpdeskspringangular.services;
+
+import com.arthur.helpdeskspringangular.domain.Tecnico;
+import com.arthur.helpdeskspringangular.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
