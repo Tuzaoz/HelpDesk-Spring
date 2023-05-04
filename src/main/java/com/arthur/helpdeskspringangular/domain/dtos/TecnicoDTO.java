0d40af4c1aa880053dfe7cd20adbff48dbec57d9
Arthur Dantas <arthurdantaspro@gmail.com>
2023-05-04 20:24:58 -0300

    Criado TecnicoDTO

---
+++ src/main/java/com/arthur/helpdeskspringangular/domain/dtos/TecnicoDTO.java
@@ -0,0 +1,98 @@
+package com.arthur.helpdeskspringangular.domain.dtos;
+
+import com.arthur.helpdeskspringangular.domain.Tecnico;
+import com.arthur.helpdeskspringangular.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.FetchType;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TecnicoDTO implements Serializable {
+    private static final long serialVersionUID=1L;
+    protected Integer id;
+    protected String nome;
+    protected String cpf;
+    protected String email;
+    protected String senha;
+
+    protected Set<Integer> perfil = new HashSet<>();
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCricao = LocalDate.now();
+
+    public TecnicoDTO() {
+    }
+
+    public TecnicoDTO(Tecnico obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfil = obj.getPerfil().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCricao = obj.getDataCricao();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<Perfil> getPerfil() {
+        return perfil.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil.add(perfil.getCodigo());
+    }
+
+    public LocalDate getDataCricao() {
+        return dataCricao;
+    }
+
+    public void setDataCricao(LocalDate dataCricao) {
+        this.dataCricao = dataCricao;
+    }
+}
